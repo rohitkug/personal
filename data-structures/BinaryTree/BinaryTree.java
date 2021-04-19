cae04b2d1c314471312fbe5514643c7a07c3294f
rohit.8655@gmail.com <rohit.8655@gmail.com>
2021-04-19 22:33:53 +0530

    Count ways of reaching nth stair

---
+++ data-structures/BinaryTree/BinaryTree.java
@@ -0,0 +1,123 @@
+import java.util.*;
+
+public class BinaryTree<T> {
+    private Node<T> root;
+    private int size;
+
+    public void add(T data) {
+        Node<T> node = new Node<>(data);
+        if(root==null) {
+            root=node;
+            root.parent=root;//root is its own parent
+            size++;
+            return;
+        }
+        //find parent node where insertion will happen
+        Node<T> parent = findNextInsertionPosition(root);
+        //no need for null check as we'll always find a position to insert
+        if(parent.left==null)//insert on left first
+            parent.left=node;
+        else parent.right=node;
+        node.parent=parent;
+        size++;
+    }
+    public boolean remove(T data) throws Exception {
+        /*
+            removal only supported for leaf nodes in this example
+            assumes there are no duplicates in tree
+         */
+        Node<T> node = findNode(data);
+        if(node==null)
+            throw new NoSuchElementException();
+        if(node.left!=null || node.right!=null)
+            throw new Exception("Deletion of non-leaf nodes not supported");
+        if(node==root) {
+            root=null;
+            root.parent=null;
+            root.data=null;
+            size--;
+            node=null;
+            return true;
+        }
+        if(node.parent.left==node)
+            node.parent.left=null;
+        else
+            node.parent.right=null;
+        node.parent=null;
+        node.data=null;
+        node=null;
+        size--;
+        return true;
+    }
+
+    private Node<T> findNode(T data) {
+        if(root==null)
+            return null;
+        Queue<Node<T>> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()) {
+            Node<T> tmp = q.remove();
+            if(tmp.data.equals(data))
+                return tmp;
+            if(tmp.left!=null) q.add(tmp.left);
+            if(tmp.right!=null) q.add(tmp.right);
+        }
+        return null;
+    }
+
+    private Node<T> findNextInsertionPosition(Node<T> root) {
+        Queue<Node<T>> q = new LinkedList<Node<T>>();
+        q.add(root);
+        while(!q.isEmpty()) {
+            Node<T> tmp = q.remove();
+            if(tmp.left==null || tmp.right==null)
+                return tmp;
+            if(tmp.left!=null)  q.add(tmp.left); //will always be true
+            if(tmp.right!=null) q.add(tmp.right);//will always be true
+        }
+        return null;
+    }
+    private List<T> getLeftView() {
+        if(root==null) return Collections.emptyList();
+        List<T> ret = new ArrayList<>();
+        Queue<Node<T>> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            Node<T> tmp = q.remove();
+            ret.add(tmp.data);
+            if(tmp.left!=null)
+                q.add(tmp.left);
+            else if(tmp.right!=null)
+                q.add(tmp.right);
+        }
+        return ret;
+    }
+    private static class Node<T> {
+        T data;
+        Node<T> left;
+        Node<T> right;
+        Node<T> parent;
+        Node(T data) {
+            this.data=data;
+            this.left=this.right=this.parent=null;
+        }
+        /*Node(T data, Node<T> left, Node<T> right, Node<T> parent) {
+            this.data = data;
+            this.left = left;
+            this.right=right;
+            this.parent=parent;
+        }*/
+    }
+    public static void main(String[] args) throws Exception {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.add(1); tree.add(2); tree.add(3); tree.add(4);
+        tree.add(5); tree.remove(4);
+        List<Integer> leftView = tree.getLeftView();
+        System.out.println("Size="+tree.size());
+        System.out.println("Left View = "+leftView);
+    }
+
+    private int size() {
+        return this.size;
+    }
+}
---
+++ data-structures/BinaryTree/README.md
@@ -0,0 +1,5 @@
+Binary Tree (not Binary Search Tree)
+Created only for trying out traversal problems
+Supports only insertion at next available left-most position
+Supports only deletion of leaf nodes
+Works as expected only if there are no duplicate nodes
